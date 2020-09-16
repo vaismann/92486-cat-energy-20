b5a317edbf33015f2e13f2f9114e7828457d3a34
vaismann <vassiliartemjev@gmail.com>
2020-09-16 15:27:47 +0300

    Финальные правки

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,8 @@
   var navMain = document.querySelector('.header-nav');
   var navToggle = document.querySelector('.header-nav__toggle');
+  var contactsMap = document.querySelector('.cooperation__map');
+
+  contactsMap.classList.remove('cooperation__map--nojs');
 
   navMain.classList.remove('header-nav--nojs');
 
